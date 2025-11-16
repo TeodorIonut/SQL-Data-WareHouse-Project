a54e3a8ae66afbca5b670f370921d109e0a0cceb
TeodorIonut <popitrockstar@gmail.com>
2025-11-16 10:21:12 +0200

    Update init-database.sql

--- scripts/init-database.sql
+++ scripts/init-database.sql
@@ -1,1 +1,25 @@
+USE master;
+GO;
+
+
+IF EXISTS(SELECT 1 FROM sys.databases WHERE name = 'DataWareHouse')
+
+BEGIN	
+	ALTER DATABASE DataWareHouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+	DROP DATABASE DataWareHouse;
+END;
+GO
+
+--create database
+CREATE DATABASE DataWareHouse;
+GO
+USE DataWareHouse;
+GO
+--#1. create the schemas
+
+CREATE SCHEMA bronze;
+GO
+CREATE SCHEMA silver;
+GO
+CREATE SCHEMA gold;
 
